4e88cc6ee8a019af4e4e459e994fe67d26dde103
James Utley <james.utley@syndicate-labs.io>
2024-08-31 09:11:50 -0500

    update for testing

--- scrapedaweb4.py
+++ scrapedaweb4.py
@@ -16,6 +16,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 from openai import OpenAI
 
